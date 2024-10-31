a013cd4ddc57fbcdb93faa0bd5855e364596ddc1
Marharyta Malets <marharyta_malets@epam.com>
2024-10-31 19:40:08 +0300

    feat: add spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import './App.css';
 import { PureComponent } from 'react';
-import Main from './components/main/Main';
 import Header from './components/header/Header';
+import Main from './components/main/Main';
+import Spinner from './components/spinner/Spinner';
 import People from './services/SwapService';
 import { IPeople } from './models/people';
+import './App.css';
 
-type State = { searchInput: string; people: IPeople };
+type State = { searchInput: string; people: IPeople; isLoading: boolean };
 type Props = unknown;
 
 class App extends PureComponent<Props, State> {
   constructor(props: Props) {
     super(props);
 
-    this.state = { searchInput: '', people: {} as IPeople };
+    this.state = { searchInput: '', people: {} as IPeople, isLoading: false };
     this.handleClick = this.handleClick.bind(this);
   }
 
   async componentDidMount() {
+    this.setState((prev) => ({ ...prev, isLoading: true }));
     const searchInput = localStorage.getItem('searchInput');
 
-    await People.getPeople(searchInput).then((res) => this.setState({ searchInput: searchInput ?? '', people: res }));
+    await People.getPeople(searchInput).then((res) =>
+      this.setState({ searchInput: searchInput ?? '', people: res, isLoading: false })
+    );
   }
 
   async handleClick(search: string) {
@@ -27,10 +31,12 @@ class App extends PureComponent<Props, State> {
     const trimSearch = search.trim();
 
     if (searchInput !== trimSearch) {
-      this.setState(() => ({ searchInput: trimSearch }));
+      this.setState((prev) => ({ ...prev, isLoading: true }));
       localStorage.setItem('searchInput', trimSearch);
 
-      await People.getPeople(trimSearch).then((res) => this.setState({ searchInput: trimSearch ?? '', people: res }));
+      await People.getPeople(trimSearch).then((res) =>
+        this.setState({ searchInput: trimSearch ?? '', people: res, isLoading: false })
+      );
     }
   }
 
@@ -38,12 +44,13 @@ class App extends PureComponent<Props, State> {
     const {
       searchInput,
       people: { results },
+      isLoading,
     } = this.state;
 
     return (
       <>
         <Header defaultValue={searchInput} handleClick={this.handleClick} />
-        <Main list={results ?? []} />
+        {isLoading ? <Spinner /> : <Main list={results ?? []} />}
       </>
     );
   }
