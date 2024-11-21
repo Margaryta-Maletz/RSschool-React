0d0cf4f779914d92d95d687bba923ea74a734d73
Marharyta Malets <marharyta_malets@epam.com>
2024-11-21 20:54:33 +0300

    feat: add React Router and 404 Error Page

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import ErrorPage from './components/error-page';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
